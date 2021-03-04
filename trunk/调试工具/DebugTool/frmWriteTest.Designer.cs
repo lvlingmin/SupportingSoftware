5fbb28cf6b0ac6f4ba45591e5cf9bd41d3b6a772
735320608@qq.com <735320608@qq.com@a5f432f4-26d5-4b09-b989-798e4fcd4a1c>
2021-03-04 03:35:46 +0000

    修改泵阀、E2测试工装

--- trunk/调试工具/DebugTool/frmWriteTest.Designer.cs
+++ trunk/调试工具/DebugTool/frmWriteTest.Designer.cs
@@ -33,16 +33,20 @@
             this.num1 = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
             this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.num1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnWriteIn
             // 
-            this.btnWriteIn.Location = new System.Drawing.Point(91, 114);
+            this.btnWriteIn.Location = new System.Drawing.Point(115, 223);
             this.btnWriteIn.Name = "btnWriteIn";
             this.btnWriteIn.Size = new System.Drawing.Size(108, 48);
             this.btnWriteIn.TabIndex = 0;
-            this.btnWriteIn.Text = "写入";
+            this.btnWriteIn.Text = "执行";
             this.btnWriteIn.UseVisualStyleBackColor = true;
             this.btnWriteIn.Click += new System.EventHandler(this.btnWriteIn_Click);
             // 
@@ -56,19 +60,19 @@
             // 
             // num1
             // 
-            this.num1.Location = new System.Drawing.Point(136, 62);
+            this.num1.Location = new System.Drawing.Point(126, 154);
             this.num1.Maximum = new decimal(new int[] {
             65000,
             0,
             0,
             0});
             this.num1.Minimum = new decimal(new int[] {
-            1,
+            10000,
             0,
             0,
-            0});
+            -2147483648});
             this.num1.Name = "num1";
-            this.num1.Size = new System.Drawing.Size(84, 21);
+            this.num1.Size = new System.Drawing.Size(129, 21);
             this.num1.TabIndex = 2;
             this.num1.Value = new decimal(new int[] {
             1,
@@ -79,7 +83,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(89, 64);
+            this.label1.Location = new System.Drawing.Point(79, 156);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 3;
@@ -95,11 +99,60 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "模块：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "功能：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "通信板\t\t",
+            "清洗板\t\t",
+            "计数器板\t",
+            "理杯机板\t",
+            "加样板\t\t",
+            "液位探测板\t",
+            "加热板\t\t",
+            "制冷板\t\t",
+            "温育板"});
+            this.comboBox1.Location = new System.Drawing.Point(126, 61);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(129, 20);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(126, 106);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(129, 20);
+            this.comboBox2.TabIndex = 8;
+            // 
             // frmWriteTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.num1);
@@ -108,7 +161,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmWriteTest";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "通讯板写入测试";
+            this.Text = "泵阀&E2测试";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmWriteTest_FormClosed);
             this.Load += new System.EventHandler(this.frmWriteTest_Load);
             ((System.ComponentModel.ISupportInitialize)(this.num1)).EndInit();
@@ -124,5 +177,9 @@
         private System.Windows.Forms.NumericUpDown num1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
--- trunk/调试工具/DebugTool/frmWriteTest.cs
+++ trunk/调试工具/DebugTool/frmWriteTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,8 +12,18 @@ namespace DebugTool
 {
     public partial class frmWriteTest : Form
     {
+        List<string> funcList1 = new List<string>();
+        List<string> funcList2 = new List<string>();
+        List<string> funcList3 = new List<string>();
+        List<string> funcList4 = new List<string>();
+        List<string> funcList5 = new List<string>();
         private string hData = "";
         private int num = 0;
+        string item;
+        string itemIndex;
+        string func ;
+        string funcIndex;
+        int paraValue;
         public frmWriteTest()
         {
             InitializeComponent();
@@ -21,20 +32,73 @@ namespace DebugTool
         private void frmWriteTest_Load(object sender, EventArgs e)
         {
             NetCom3.Instance.ReceiveHandel += dealOrder;
+            funcList1.Add("E2写入测试");
+
+            funcList2.Add("E2写入测试");
+            funcList2.Add("蠕动泵");
+            funcList2.Add("计量泵");
+            funcList2.Add("混匀电机");
+
+            funcList3.Add("E2写入测试");
+            funcList3.Add("读数");
+
+            funcList4.Add("E2写入测试");
+            funcList4.Add("齿轮泵");
+            funcList4.Add("隔膜泵");
+            funcList4.Add("电磁阀");
+            funcList4.Add("托马斯泵");
+
+            funcList5.Add("E2写入测试");
+            funcList5.Add("混匀电机");
         }
 
         private void btnWriteIn_Click(object sender, EventArgs e)
         {
+            if(num1.Value == 0)
+            {
+                MessageBox.Show("参数不允许是零！");
+                return;
+            }
             btnWriteIn.Enabled = false;
-           
-            num++;
-            hData = "";
-            hData = int.Parse(num1.Value.ToString()).ToString("X4");
-            hData = hData.Insert(2 , " ");
+            #region para
+            item = comboBox1.Text.Trim();
+            itemIndex = (comboBox1.SelectedIndex + 1).ToString("X2");
+            func = comboBox2.Text;
+            funcIndex = (comboBox2.SelectedIndex).ToString("X2");
+            paraValue = (int)num1.Value;
 
-            NetCom3.Instance.Send(NetCom3.Cover("EB 90 F0 01 " + hData) , 5);
-            NetCom3.Instance.SingleQuery();
-            
+            #endregion
+
+            #region
+            num++;
+            hData = paraValue.ToString("x8");
+
+            #endregion
+
+            #region
+            if (func.Contains("E2"))
+            {
+                hData = "";
+                hData = int.Parse(num1.Value.ToString()).ToString("X4");
+                hData = hData.Insert(2, " ");
+                NetCom3.Instance.Send(NetCom3.Cover("EB 90 F0 01 " + itemIndex + " 00 " + hData), 5);
+                NetCom3.Instance.SingleQuery();
+            }
+            else if(item.Contains("计数器板"))
+            {
+                NetCom3.Instance.Send(NetCom3.Cover("EB 90 F0 01 " + itemIndex + " " + funcIndex), 5);
+                NetCom3.Instance.SingleQuery();
+                //deal read num
+            }
+            else
+            {
+                NetCom3.Instance.Send(NetCom3.Cover("EB 90 F0 01 " + itemIndex + " " + funcIndex + " " + hData.Substring(0, 2) + " " + hData.Substring(2, 2) + " "
+                      + hData.Substring(4, 2) + " " + hData.Substring(6, 2)), 5);
+                NetCom3.Instance.SingleQuery();
+            }
+           
+            #endregion
+
             btnWriteIn.Enabled = true;
         }
 
@@ -44,28 +108,58 @@ namespace DebugTool
             {
                 int tempInt = order.IndexOf("EB 90 01 A0 ");
                 string pra = order.Substring(tempInt + 12, 5);
-
-                string temp;
-                if (pra == hData)
+                string pra2 = order.Substring(tempInt + 36, 11);
+                string temp = "";
+                if (func.Contains("E2"))//e2测试
                 {
-                    temp = "写入成功！";
+                    if (pra == hData)
+                    {
+                        temp = "写入成功！";
+                    }
+                    else if (pra == "00 00")
+                    {
+                        temp = "没有返回参数！";
+                    }
+                    else
+                    {
+                        temp = "返回参数错误！";
+                    }
                 }
-                else if (pra == "00 00")
+                else if(func.Contains("读数"))//计数板读数
                 {
-                    temp = "没有返回参数！";
+                    pra2 = pra2.Replace(" ","");
+                    pra2 = Convert.ToInt64(pra2, 16).ToString();
+                    if (int.Parse(pra2) > Math.Pow(10, 5))
+                        temp = ((int)GetPMT(double.Parse(pra2))).ToString();
+                    if (int.Parse(pra2) == 0)
+                    {
+                        temp = "发光值：" + temp + ",异常";
+                    }
+                    else
+                    {
+                        temp = "发光值：" + temp;
+                    }
                 }
-                else
+                else//点击步进
                 {
-                    temp = "返回参数错误！";
+                    string sign = "";
+                    if(paraValue > 0)
+                    {
+                        sign = "+";
+                    }
+                    temp = item + "--" + func + sign + paraValue;
                 }
 
                 BeginInvoke(new Action(() =>
                 {
-                    txtResultShow.AppendText("\n(" + num  +") " + DateTime.Now.ToString("hh:mm:ss-> ") + temp);
+                    txtResultShow.AppendText("\n(" + num + ") " + DateTime.Now.ToString("hh:mm:ss-> ") + temp);
                 }));
             }
         }
-
+        public double GetPMT(double pmt)
+        {
+            return pmt = pmt / (1 - pmt * 20 * Math.Pow(10, -9)); ;
+        }
         private void frmWriteTest_FormClosed(object sender, FormClosedEventArgs e)
         {
             NetCom3.Instance.ReceiveHandel -= dealOrder;
@@ -75,6 +169,30 @@ namespace DebugTool
         {
             NetCom3.Instance.ReceiveHandel -= dealOrder;
             Environment.Exit(0);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString().Contains("清洗板"))
+            {
+                comboBox2.DataSource = funcList2;
+            }
+            else if (comboBox1.SelectedItem.ToString().Contains("计数器板"))
+            {
+                comboBox2.DataSource = funcList3;
+            }
+            else if (comboBox1.SelectedItem.ToString().Contains("加样板"))
+            {
+                comboBox2.DataSource = funcList4;
+            }
+            else if (comboBox1.SelectedItem.ToString().Contains("温育板"))
+            {
+                comboBox2.DataSource = funcList5;
+            }
+            else
+            {
+                comboBox2.DataSource = funcList1;
+            }
         }
     }
 }
