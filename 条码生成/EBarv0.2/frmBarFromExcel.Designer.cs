b39eaf66b537cea02adfdf3e0cadd36b7733b9a2
735320608 <735320608@qq.com>
2021-05-28 07:59:34 +0000

    添加excel录入生成

--- 条码生成/EBarv0.2/frmBarFromExcel.Designer.cs
+++ 条码生成/EBarv0.2/frmBarFromExcel.Designer.cs
@@ -29,16 +29,14 @@
         private void InitializeComponent()
         {
             this.btnImportAndExcel = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.dgvInfo = new System.Windows.Forms.DataGridView();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.batch = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.productDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.vol = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.numStart = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.createNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.btnCreate = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.data1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.data2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
             this.SuspendLayout();
             // 
             // btnImportAndExcel
@@ -51,98 +49,82 @@
             this.btnImportAndExcel.UseVisualStyleBackColor = true;
             this.btnImportAndExcel.Click += new System.EventHandler(this.btnImportAndExcel_Click);
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.id,
-            this.name,
-            this.batch,
-            this.productDate,
-            this.vol,
-            this.numStart,
-            this.createNum});
-            this.dataGridView1.Location = new System.Drawing.Point(54, 75);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(634, 275);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // id
-            // 
-            this.id.DataPropertyName = "id";
-            this.id.HeaderText = "序号";
-            this.id.Name = "id";
-            this.id.ReadOnly = true;
-            this.id.Width = 40;
-            // 
-            // name
-            // 
-            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.name.DataPropertyName = "name";
-            this.name.HeaderText = "名称";
-            this.name.Name = "name";
-            this.name.ReadOnly = true;
-            // 
-            // batch
-            // 
-            this.batch.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.batch.DataPropertyName = "batch";
-            this.batch.HeaderText = "批号";
-            this.batch.Name = "batch";
-            this.batch.ReadOnly = true;
-            // 
-            // productDate
-            // 
-            this.productDate.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.productDate.DataPropertyName = "productDate";
-            this.productDate.HeaderText = "生产时间";
-            this.productDate.Name = "productDate";
-            this.productDate.ReadOnly = true;
-            // 
-            // vol
-            // 
-            this.vol.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.vol.DataPropertyName = "vol";
-            this.vol.HeaderText = "规格";
-            this.vol.Name = "vol";
-            this.vol.ReadOnly = true;
-            // 
-            // numStart
-            // 
-            this.numStart.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.numStart.DataPropertyName = "numStart";
-            this.numStart.HeaderText = "开始编号";
-            this.numStart.Name = "numStart";
-            this.numStart.ReadOnly = true;
-            // 
-            // createNum
-            // 
-            this.createNum.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.createNum.DataPropertyName = "createNum";
-            this.createNum.HeaderText = "生成数量";
-            this.createNum.Name = "createNum";
-            this.createNum.ReadOnly = true;
-            // 
             // btnCreate
             // 
             this.btnCreate.Location = new System.Drawing.Point(593, 28);
             this.btnCreate.Name = "btnCreate";
             this.btnCreate.Size = new System.Drawing.Size(95, 30);
             this.btnCreate.TabIndex = 2;
-            this.btnCreate.Text = "生成";
+            this.btnCreate.Text = "生成与保存";
             this.btnCreate.UseVisualStyleBackColor = true;
             this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // back
+            // 
+            this.back.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.back.Location = new System.Drawing.Point(671, 372);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(75, 23);
+            this.back.TabIndex = 42;
+            this.back.Text = "返回";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // dgvInfo
+            // 
+            this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name,
+            this.data1,
+            this.data2});
+            this.dgvInfo.Location = new System.Drawing.Point(66, 93);
+            this.dgvInfo.Name = "dgvInfo";
+            this.dgvInfo.ReadOnly = true;
+            this.dgvInfo.RowTemplate.Height = 23;
+            this.dgvInfo.Size = new System.Drawing.Size(626, 234);
+            this.dgvInfo.TabIndex = 43;
+            // 
+            // id
+            // 
+            this.id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.id.DataPropertyName = "id";
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // name
+            // 
+            this.name.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.name.DataPropertyName = "name";
+            this.name.HeaderText = "参数名称";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // data1
+            // 
+            this.data1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.data1.DataPropertyName = "data1";
+            this.data1.HeaderText = "值1";
+            this.data1.Name = "data1";
+            this.data1.ReadOnly = true;
+            // 
+            // data2
+            // 
+            this.data2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.data2.DataPropertyName = "data2";
+            this.data2.HeaderText = "值2";
+            this.data2.Name = "data2";
+            this.data2.ReadOnly = true;
             // 
             // frmBarFromExcel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(758, 421);
+            this.Controls.Add(this.dgvInfo);
+            this.Controls.Add(this.back);
             this.Controls.Add(this.btnCreate);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnImportAndExcel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmBarFromExcel";
@@ -150,7 +132,7 @@
             this.Text = "条码生成&Excel";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmBarFromExcel_FormClosed);
             this.Load += new System.EventHandler(this.frmBarFromExcel_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -158,14 +140,12 @@
         #endregion
 
         private System.Windows.Forms.Button btnImportAndExcel;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.DataGridView dgvInfo;
         private System.Windows.Forms.DataGridViewTextBoxColumn id;
         private System.Windows.Forms.DataGridViewTextBoxColumn name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn batch;
-        private System.Windows.Forms.DataGridViewTextBoxColumn productDate;
-        private System.Windows.Forms.DataGridViewTextBoxColumn vol;
-        private System.Windows.Forms.DataGridViewTextBoxColumn numStart;
-        private System.Windows.Forms.DataGridViewTextBoxColumn createNum;
-        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn data1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn data2;
     }
 }
